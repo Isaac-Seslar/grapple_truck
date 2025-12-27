d961f74089aa0134c2d77e08b443ba6326d9fd9c
Isaac Seslar <56139690+Isaac-Seslar@users.noreply.github.com>
2025-12-27 15:29:27 -0700

    update car controls
    'w' and 's' act as throttle and brake where 'a' and 'd' turn

--- src/car.gd
+++ src/car.gd
@@ -6,28 +6,23 @@ const JUMP_VELOCITY = -400.0
 
 var max_speed := 1200.0
 var vel := Vector2(0, 0)
-var steering_factor := 3.0
+var steering_factor := 5.0
+var acceleration_factor := 0.3
 
 
 func _process(delta: float) -> void:
+	var throttle := Input.get_axis("accel","decel")
+	var rotation_direction := Input.get_axis("turn_left", "turn_right")
 	
-	var direction := Vector2(0, 0)
-	direction.x = Input.get_axis("move_left", "move_right")
-	direction.y = Input.get_axis("move_up", "move_down")
-	
-
-	if direction.length() > 1.0:
-		direction = direction.normalized()
-		
-
-	var desired_velocity := max_speed * direction
+	rotation += rotation_direction * delta
+	var direction := Vector2.from_angle(rotation)
+	var desired_velocity := max_speed * direction * throttle
 	var steering := desired_velocity - vel
-	vel += steering * steering_factor * delta
+	vel += steering * steering_factor * acceleration_factor * delta 
 	position += vel * delta
-
-	if vel.length() > 0.0:
-		get_node("Sprite2D").rotation = vel.angle()
-		
+	
+	print(vel)
+	
 	var viewport_size := get_viewport_rect().size
 	position.x = wrapf(position.x, 0, viewport_size.x)
 	position.y = wrapf(position.y, 0, viewport_size.y)
